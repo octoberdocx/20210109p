0e7ee31f5dfea5c6e97d0dba8bde42021310122a
octoberdocx <77180954+octoberdocx@users.noreply.github.com>
2021-01-09 10:55:40 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let weapon = 0
+input.onGesture(Gesture.Shake, function () {
+    weapon = randint(0, 2)
+    if (weapon == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (weapon == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else if (weapon == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
